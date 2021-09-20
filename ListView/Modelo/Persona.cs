0799ac553292f56458a94d32138c91b7b3a94b27
RokieTheJawa <lchavezx@ucol.mx>
2021-09-20 14:25:46 -0500

    Agregado Modelo Agregar archivos de proyecto.

---
+++ ListView/Modelo/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListView.Modelo
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+    }
+}
