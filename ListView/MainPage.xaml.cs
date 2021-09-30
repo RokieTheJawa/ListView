7aa3a0bafe8d84918dabd05ba471ba938c89f08c
RokieTheJawa <72667052+RokieTheJawa@users.noreply.github.com>
2021-09-29 19:52:24 -0500

    Validaciones

--- ListView/MainPage.xaml.cs
+++ ListView/MainPage.xaml.cs
@@ -9,64 +9,81 @@ using Xamarin.Forms;
 
 namespace ListView
 {
-//test
+
     public partial class MainPage : ContentPage
     {
         public List<Persona> Personas { get; set; }
+
+        public bool seleccionado = false;
 
         public int indice;
         public MainPage()
         {
             InitializeComponent();
-
             Personas = new List<Persona>();
 
             Personas.Add(new Persona() { Nombre = "Persona 1", Correo = "correo1@ucol.mx", Telefono = "1234224232" });
-            //Personas.Add(new Persona() { Nombre = "Persona 2", Correo = "correo2@ucol.mx" });
-            //Personas.Add(new Persona() { Nombre = "Persona 3", Correo = "correo3@ucol.mx" });
-            //Personas.Add(new Persona() { Nombre = "Persona 4", Correo = "correo4@ucol.mx" });
-            //Personas.Add(new Persona() { Nombre = "Persona 5", Correo = "correo5@ucol.mx" });
+            Personas.Add(new Persona() { Nombre = "Persona 2", Correo = "correo2@ucol.mx", Telefono = "1234224232" });
+            Personas.Add(new Persona() { Nombre = "Persona 3", Correo = "correo3@ucol.mx", Telefono = "1234224232" });
+            Personas.Add(new Persona() { Nombre = "Persona 4", Correo = "correo4@ucol.mx", Telefono = "1234224232" });
+            Personas.Add(new Persona() { Nombre = "Persona 5", Correo = "correo5@ucol.mx", Telefono = "1234224232" });
 
             lstPersonas.ItemsSource = Personas;
 
-            //Nombres = new List<string>();
-            //Nombres.Add("Nombre 1");
-            //Nombres.Add("Nombre 2");
-            //Nombres.Add("Nombre 3");
-            //Nombres.Add("Nombre 4");
         }
 
         private void Boton_Agregar(object sender, EventArgs e)
         {
-            //lblMensaje.Text = "Hola " + txtNombre.Text;
-
-            Personas.Add(new Persona() { Nombre = txtNombre.Text, Correo = txtCorreo.Text, Telefono = txtTelefono.Text });
-
-            lstPersonas.ItemsSource = null;
-            lstPersonas.ItemsSource = Personas;
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtCorreo.Text) || string.IsNullOrEmpty(txtTelefono.Text))
+            {
+                DisplayAlert("Error", "Los campos no deben estar vacíos", "Aceptar");
+            }
+            else
+            {
+                Personas.Add(new Persona() { Nombre = txtNombre.Text, Correo = txtCorreo.Text, Telefono = txtTelefono.Text });
+                lstPersonas.ItemsSource = null;
+                lstPersonas.ItemsSource = Personas;
+                seleccionado = false;
+            }
         }
 
-        private void Boton_Modificar (object sender, EventArgs e)
+        private void Boton_Modificar(object sender, EventArgs e)
         {
-            Personas.RemoveAt(indice);
-            Personas.Insert(indice, new Persona() { Nombre = txtNombre.Text, Correo = txtCorreo.Text, Telefono = txtTelefono.Text });
-            
-            lstPersonas.ItemsSource = null;
-            lstPersonas.ItemsSource = Personas;
+            if (seleccionado == false)
+            {
+                DisplayAlert("Error", "No hay ningún elemento seleccionado", "Aceptar");
+            }
+            else
+            {
+                Personas.RemoveAt(indice);
+                Personas.Insert(indice, new Persona() { Nombre = txtNombre.Text, Correo = txtCorreo.Text, Telefono = txtTelefono.Text });
+
+                lstPersonas.ItemsSource = null;
+                lstPersonas.ItemsSource = Personas;
+                seleccionado = false;
+            }
 
         }
 
         private void Boton_Borrar(object sender, EventArgs e)
         {
-            txtNombre.Text = "";
-            txtCorreo.Text = "";
-            txtTelefono.Text = "";
-            //lblMensaje.Text = "";
+            if (seleccionado == false)
+            {
+                DisplayAlert("Error", "Seleccione un elemento", "Aceptar");
+            }
+            else
+            {
+                txtNombre.Text = "";
+                txtCorreo.Text = "";
+                txtTelefono.Text = "";
 
-            Personas.RemoveAt(indice);
+                Personas.RemoveAt(indice);
 
-            lstPersonas.ItemsSource = null;
-            lstPersonas.ItemsSource = Personas;
+                lstPersonas.ItemsSource = null;
+                lstPersonas.ItemsSource = Personas;
+
+                seleccionado = false;
+            }
         }
 
         private void lstPersonas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -76,7 +93,7 @@ namespace ListView
             txtNombre.Text = persona.Nombre;
             txtCorreo.Text = persona.Correo;
             txtTelefono.Text = persona.Telefono;
+            seleccionado = true;
         }
-
     }
 }
